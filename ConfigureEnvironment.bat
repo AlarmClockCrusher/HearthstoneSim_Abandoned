e2d0172181b1c7196124a934abc791a03fa2009f
unknown <alarmclockcrusher@gmail.com>
2021-10-31 00:11:25 -0400

    Cleanup

--- ConfigureEnvironment.bat
+++
@@ -1,8 +0,0 @@
-Panda3D-SDK-1.10.8-x64.exe
-
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple numpy
-pip install numpy
-pip install pillow
-pip install hearthstone
-
-move panda.pth C:\Panda3D-1.10.8-x64\python\Lib\site-packages\panda.pth
